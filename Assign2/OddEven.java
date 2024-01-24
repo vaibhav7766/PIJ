b2d5e657e3ce1dc1345c7326028e50f05f91b111
vaibhav7766 <vaibhavsharmav777@gmail.com>
2024-01-24 12:29:30 +0530

    new file:   Assign2/ArrayAndArrayList.java 	new file:   Assign2/Neighbours.java 	modified:   Assign2/OddEven.java 	modified:   Assign2/UserInput.java

--- Assign2/OddEven.java
+++ Assign2/OddEven.java
@@ -1,3 +1,4 @@
+// OddEven.java
 package Assign2;
 
 import java.io.IOException;
@@ -6,18 +7,32 @@ import java.util.Scanner;
 
 public class OddEven {
     public static void main(String[] args) throws IOException {
+        // Create a Scanner object to read user input
         Scanner sc = new Scanner(System.in);
+
+        // Declare arrays for even and odd numbers
         double[] even;
         int j = 0;
         int k = 0;
         double[] odd;
-        System.out.println("Enter number of elements to enter:");
+
+        // Prompt the user to enter the number of elements
+        System.out.println("Enter the number of elements to enter:");
+
+        // Read the size from user input
         int size = sc.nextInt();
+
+        // Initialize arrays for even and odd numbers based on the user-defined size
         even = new double[size];
         odd = new double[size];
-        System.out.println("Enter the elements separated by space:");
+
+        // Get the array input from the user using the UserInput class
         double[] array = UserInput.inputArray(size);
+
+        // Close the Scanner to avoid resource leaks
         sc.close();
+
+        // Separate even and odd numbers into their respective arrays
         for (int i = 0; i < size; i++) {
             if (array[i] % 2 == 0) {
                 even[j] = array[i];
@@ -27,12 +42,17 @@ public class OddEven {
                 k++;
             }
         }
+
+        // Print even elements
         System.out.println("Even elements:");
         System.out.println(Arrays.toString(even));
+
+        // Print odd elements
         System.out.println("Odd elements:");
         System.out.print(Arrays.toString(odd));
     }
 
+    // Method to print elements of an array
     public static void print(double[] array, int size) {
         for (int i = 0; i < size; i++) {
             System.out.println(array[i]);
